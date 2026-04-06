3ffed99ef0712a12d567be1c7988af6348281399
fryordy-boy <edgar.estonia@gmail.com>
2026-04-06 17:36:07 +0300

    dummy commit

--- backend/server.ts
+++ backend/server.ts
@@ -7,22 +7,56 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const books = [
+interface Book {
+    id: number,
+    title: string,
+    author?: string
+}
+// здесь не заьудь в коде прописать этот ифчик
+
+type BooksArray = Book[]
+
+interface GetBooks {
+    book: string
+}
+
+interface AddBookRequest {
+    title: string,
+    author: string
+}
+
+interface DeleteBookRequest {
+    id: number
+}
+
+interface ApiResponse<T> {
+    result: "ok"|"error",
+    message?: string,
+    data?: T
+}
+
+interface booksData {
+    id: string,
+    title: string,
+    author: string
+}
+
+const books: BooksArray = [
     { id: 1, title: 'Book 1', author: 'Author 1' },
     { id: 2, title: 'Book 2', author: 'Author 2' },
-    { id: 3, title: 'Book 3', author: 'Author 3' },
+    { id: 3, title:  'Book 3', author: 'Author 3' },
 ];
 
-app.get('/books', (req, res) => {
+app.get('/books', (req: Request<{}, any, any, ParsedQs, Record<string, any>>, res:ApiResponse<booksData>) => {
     res.send(books);
 });
 
-app.get('/books/:id', (req, res) => {
-    const book = books.find(b => parseInt(b.id) === parseInt(req.params.id));
+app.get('/books/:id', (req: Request<{}, any, any, ParsedQs, Record<string, any>>, res:ApiResponse<booksData>) => {
+    const book: GetBooks = books.find((b) => parseInt(b.id) === parseInt(req.params.id));
     res.status(200).send({ bookFromResponse: book, result: 'ok' });
 });
 
-app.post('/books/add', (req, res) => {
+app.post('/books/add', (req: Request<{}, {}, AddBookRequest>, res) => {
     const { title, author } = req.body;
     if (!title || !author) return res.status(400).send({ result: 'error', message: 'Title and author are required' });
 
@@ -31,7 +65,7 @@ app.post('/books/add', (req, res) => {
     res.status(200).send({ result: 'ok', newBook });
 });
 
-app.put('/books/edit/:id', (req, res) => {
+app.put('/books/edit/:id', (req, res:ApiResponse<booksData>) => {
     const { title, author } = req.body;
     const book = books.find(b => parseInt(b.id) === parseInt(req.params.id));
     if (!book) return res.status(404).send({ result: 'error', message: 'Book not found' });
@@ -41,7 +75,7 @@ app.put('/books/edit/:id', (req, res) => {
     res.status(200).send({ result: 'ok', book });
 });
 
-app.delete('/books/delete', (req, res) => {
+app.delete('/books/delete', (req:ApiResponse<booksData>: Request<{}, {}, DeleteBookRequest>, res) => {
     const { id } = req.body;
     const index = books.findIndex(b => parseInt(b.id) === parseInt(id));
     if (index === -1) return res.status(404).send({ result: 'error', message: 'Book not found' });
